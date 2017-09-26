c2d65a8d1a8515379cd3f5bc78f06d9c2a419560
598185 <a598185@TCSC02SK6E8G8WN.local>
2017-09-26 15:12:42 +0530

    fullfillment test5

--- index.js
+++ index.js
@@ -23,9 +23,9 @@ app.post('/helloHttp', function(req, res) {
   response = "This is a sample response from your webhook!" //Default response from the webhook to show it's working
   //console.log("Request: ",req);
   console.log('Request headers: ' + JSON.stringify(req.headers));
-  console.log('Request body: ' + JSON.stringify(req.originalRequest));
+  console.log('Request body: ' + JSON.stringify(req));
   res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
-  res.send(JSON.stringify({ "speech": response, "displayText": response
+  res.send(JSON.stringify({ "speech": req, "displayText": response
   //"speech" is the spoken version of the response, "displayText" is the visual version
   }));
 });
